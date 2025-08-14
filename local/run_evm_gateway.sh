bc007c88db2c7eab72b9bfbfd3386d4d4a7e98fd
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2025-08-14 16:49:57 -0400

    run service

--- local/run_evm_gateway.sh
+++ local/run_evm_gateway.sh
@@ -1,6 +1,6 @@
 EMULATOR_COINBASE=FACF71692421039876a5BB4F10EF7A439D8ef61E
 EMULATOR_COA_ADDRESS=e03daebed8ca0615
-EMULATOR_COA_KEY=$(cat ./evm-gateway.pkey)
+EMULATOR_COA_KEY=$(cat ./local/evm-gateway.pkey)
 
 cd ./lib/flow-evm-gateway/
 rm -rf db/
@@ -16,5 +16,13 @@ CGO_ENABLED=1 go run cmd/main.go run \
 	--tx-state-validation=local-index \
 	--profiler-enabled=true \
 	--profiler-port=6060 \
-	--ws-enabled=true
+	--ws-enabled=true &
 
+# Port to check
+PORT=8545
+
+# Wait for port to be available
+echo "Waiting for port $PORT to be ready..."
+while ! nc -z localhost $PORT; do
+  sleep 1
+done
