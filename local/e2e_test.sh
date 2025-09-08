c25b505512fd01cf359ec0902f46cb10cfc51028
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2025-09-08 11:01:12 -0400

    fix e2e

--- local/e2e_test.sh
+++ local/e2e_test.sh
@@ -1,20 +1,25 @@
-echo_info "Grant Protocol Beta access to TidalYield..."
+echo "Grant Protocol Beta access to TidalYield..."
 flow transactions send ./lib/TidalProtocol/cadence/tests/transactions/tidal-protocol/pool-management/03_grant_beta.cdc --authorizer emulator-account,emulator-account --proposer emulator-account --payer emulator-account
 
-echo_info "Grant Tide Beta access to test user..."
+echo "Grant Tide Beta access to test user..."
 flow transactions send ./cadence/transactions/tidal-yield/admin/grant_beta.cdc --authorizer emulator-account,test-user --proposer test-user --payer emulator-account
 
-echo_info "Creating Tide[0]..."
+echo "Transfer Flow tokens"
+flow transactions send "./cadence/transactions/flow-token/transfer_flow.cdc" 0x179b6b1cb6755e31 1000.0
+
+#flow transactions send "./cadence/transactions/yield-token/setup_vault.cdc" --signer test-user
+
+echo "Creating Tide[0]..."
 flow transactions send ./cadence/transactions/tidal-yield/create_tide.cdc \
   A.f8d6e0586b0a20c7.TidalYieldStrategies.TracerStrategy \
   A.0ae53cb6e3f42a79.FlowToken.Vault \
   100.0 \
   --signer test-user
 
-echo_info "Depositing 20.0 to Tide[0]..."
+echo "Depositing 20.0 to Tide[0]..."
 flow transactions send ./cadence/transactions/tidal-yield/deposit_to_tide.cdc 0 20.0 --signer test-user
 
-echo_info "Withdrawing 10.0 from Tide[0]..."
+echo "Withdrawing 10.0 from Tide[0]..."
 flow transactions send ./cadence/transactions/tidal-yield/withdraw_from_tide.cdc 0 10.0 --signer test-user
 
 flow transactions send ./cadence/transactions/tidal-yield/close_tide.cdc 0 --signer test-user
