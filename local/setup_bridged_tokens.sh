c4491656ad25a8b4d4c5101a36d690ef4699f2c7
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2025-10-27 09:50:49 -0400

    fix local token bridging

--- local/setup_bridged_tokens.sh
+++ local/setup_bridged_tokens.sh
@@ -1,5 +1,9 @@
-# bridge USDC
+# bridge USDC to Cadence
 flow transactions send ./lib/flow-evm-bridge/cadence/transactions/bridge/onboarding/onboard_by_evm_address.cdc 0xaCCF0c4EeD4438Ad31Cd340548f4211a465B6528 --signer emulator-account --gas-limit 9999
 
-# bridge WBTC 
+# bridge WBTC to Cadence
 flow transactions send ./lib/flow-evm-bridge/cadence/transactions/bridge/onboarding/onboard_by_evm_address.cdc 0x374BF2423c6b67694c068C3519b3eD14d3B0C5d1 --signer emulator-account --gas-limit 9999
+
+# bridge MOET to EVM
+flow transactions send ./lib/flow-evm-bridge/cadence/transactions/bridge/onboarding/onboard_by_type_identifier.cdc "A.f3fcd2c1a78f5eee.MOET.Vault" --signer emulator-account --gas-limit 9999
+
--- local/setup_emulator.sh
+++ local/setup_emulator.sh
@@ -3,6 +3,10 @@ git submodule update --init --recursive
 # execute emulator deployment
 flow deps install --skip-alias --skip-deployments
 flow deploy
+flow deploy
+
+flow transactions send ./cadence/transactions/moet/setup_vault.cdc
+flow transactions send ./cadence/transactions/moet/mint_moet.cdc 0xf8d6e0586b0a20c7 1000000.0 --signer mock-incrementfi
 
 # set mocked prices in the MockOracle contract, initialized with MOET as unitOfAccount
 flow transactions send ./cadence/transactions/mocks/oracle/set_price.cdc 'A.0ae53cb6e3f42a79.FlowToken.Vault' 0.5
@@ -11,7 +15,7 @@ flow transactions send ./cadence/transactions/mocks/oracle/set_price.cdc 'A.f8d6
 # configure TidalProtocol
 #
 # create Pool with MOET as default token
-flow transactions send ./cadence/transactions/tidal-protocol/pool-factory/create_and_store_pool.cdc 'A.f8d6e0586b0a20c7.MOET.Vault'
+flow transactions send ./cadence/transactions/tidal-protocol/pool-factory/create_and_store_pool.cdc 'A.f3fcd2c1a78f5eee.MOET.Vault'
 # add FLOW as supported token - params: collateralFactor, borrowFactor, depositRate, depositCapacityCap
 flow transactions send ./cadence/transactions/tidal-protocol/pool-governance/add_supported_token_simple_interest_curve.cdc \
     'A.0ae53cb6e3f42a79.FlowToken.Vault' \
@@ -24,7 +28,7 @@ flow transactions send ./cadence/transactions/tidal-protocol/pool-governance/add
 # 
 # wire up liquidity to MockSwapper, mocking AMM liquidity sources
 flow transactions send ./cadence/transactions/mocks/swapper/set_liquidity_connector.cdc /storage/flowTokenVault
-flow transactions send ./cadence/transactions/mocks/swapper/set_liquidity_connector.cdc /storage/moetTokenVault_0xf8d6e0586b0a20c7
+flow transactions send ./cadence/transactions/mocks/swapper/set_liquidity_connector.cdc /storage/moetTokenVault_0xf3fcd2c1a78f5eee
 flow transactions send ./cadence/transactions/mocks/swapper/set_liquidity_connector.cdc /storage/yieldTokenVault_0xf8d6e0586b0a20c7
 # add TracerStrategy as supported Strategy with the ability to initialize when new Tides are created
 flow transactions send ./cadence/transactions/tidal-yield/admin/add_strategy_composer.cdc \
