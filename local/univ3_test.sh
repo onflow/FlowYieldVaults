b2b543dcf3abb765c1d137f71e5898e40821b496
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2025-10-29 21:09:11 -0400

    remove unused scripts

--- local/univ3_test.sh
+++ local/univ3_test.sh
@@ -8,11 +8,6 @@ echo "setup PunchSwap"
 
 ./local/punchswap/setup_punchswap.sh
 
-# echo "Setup EVM bridge"
-#
-# forge script ./solidity/script/01_DeployBridge.s.sol:DeployBridge \
-#   --rpc-url http://127.0.0.1:8545 --broadcast --legacy --gas-price 0 --slow
-
 ./local/punchswap/e2e_punchswap.sh
 
 echo "Setup emulator"
@@ -20,10 +15,3 @@ echo "Setup emulator"
 
 ./local/setup_bridged_tokens.sh
 
-#
-# CODE_HEX=$(xxd -p -c 200000 ./cadence/contracts/PunchSwapV3Connector.cdc)
-# flow transactions send ./cadence/tx/deploy_punchswap_connector.cdc \
-#   --network emulator \
-#   --signer emulator-account \
-#   --arg String:PunchSwapV3Connector \
-#   --arg String:$CODE_HEX
