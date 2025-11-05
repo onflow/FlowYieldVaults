5527a58cc686e72a36fae1d84983d4e9efd237e7
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2025-11-05 08:18:03 -0500

    move deps install

--- local/setup_wallets.sh
+++ local/setup_wallets.sh
@@ -1,3 +1,5 @@
+flow deps install --skip-alias --skip-deployments
+
 TEST_USER_PUBKEY_PATH="./local/test-user.pubkey"
 AMM_PUBKEY_PATH="./local/mock-incrementfi.pubkey"
 EVM_GATEWAY_PUBKEY_PATH="./local/evm-gateway.pubkey"
@@ -15,4 +17,4 @@ flow transactions send "./cadence/transactions/flow-token/transfer_flow.cdc" 0xe
 
 # tidal
 echo "fund tidal"
-flow transactions send "./cadence/transactions/flow-token/transfer_flow.cdc" 0x045a1763c93006ca 1000.0
+flow transactions send "./cadence/transactions/flow-token/tragnsfer_flow.cdc" 0x045a1763c93006ca 1000.0
