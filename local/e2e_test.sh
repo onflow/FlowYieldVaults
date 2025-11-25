c323ec6f649cff03f508455820b53b8dd7c4bc05
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2025-11-25 10:25:27 -0700

    Refactor tests to use test env embedded VM Bridge (#90)
    * refactor tests to use test env embedded VM Bridge
    
    * fix failing e2e tests
    
    * rename instances of tidal -> flow vaults
    
    * fix failing increment-fi e2e tests
    
    * extend emulator transaction expiry for forge script evm env setup
    
    * fix e2e_test script signer aliases

--- local/e2e_test.sh
+++ local/e2e_test.sh
@@ -21,9 +21,9 @@ run_txn() {
 
 run_txn "Grant Tide Beta access to test user" \
   ./cadence/transactions/flow-vaults/admin/grant_beta.cdc \
-  --authorizer tidal,test-user \
+  --authorizer emulator-flow-vaults,test-user \
   --proposer test-user \
-  --payer tidal
+  --payer emulator-flow-vaults
 
 run_txn "Transfer Flow tokens" \
   ./cadence/transactions/flow-token/transfer_flow.cdc \
